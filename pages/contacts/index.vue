0a2a52d27f91198971b8fde134c0090eb31aa02e
RunawayTatti <tatjanawitt@gmx.de>
2020-03-27 12:11:20 +0100

    wip

--- pages/contacts/index.vue
+++ pages/contacts/index.vue
@@ -3,22 +3,15 @@
     <div class="display-4 ma-4 d-flex justify-center">
       Alle Kontakte
     </div>
-    <v-sheet
-      class="mx-auto"
-      elevation="8"
-      max-width="1100"
-    >
+    <v-sheet class="mx-auto" elevation="8" max-width="1100">
       <v-slide-group
         v-model="model"
         class="pa-4"
         active-class="success"
         show-arrows
       >
-        <v-slide-item
-          v-for="contact in contacts"
-          :key="contact.id"
-        >
-          <ContactItem :contact="contact" :tags="tags" />
+        <v-slide-item v-for="c in contacts" :key="c.id">
+          <ContactItem :contact="c" :tags="tags" />
         </v-slide-item>
       </v-slide-group>
     </v-sheet>
