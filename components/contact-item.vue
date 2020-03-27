976629ee39190d32e772b17cbad1ab4dd00e2335
RunawayTatti <tatjanawitt@gmx.de>
2020-03-27 12:04:19 +0100

    add contact list card-slider

---
+++ components/contact-item.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-card width="300px" hover class="ma-2">
+    <nuxt-link :to="{ path: `/contacts/detail/${contact.id}`}">
+      <v-img :src="contact.img || ''" />
+      <v-card-title>{{ contact.fName }}</v-card-title>
+      <v-card-text>{{ contact.lName }}</v-card-text>
+    </nuxt-link>
+
+    <v-card-actions>
+      <span v-for="tag_id in contact.tag_ids" :key="`${contact.id}-${tag_id}`">
+        <v-btn
+          color="orange lighten-4"
+          class="mr-2"
+          small
+        >{{ getTag(tag_id).name }}</v-btn>
+      </span>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+'use strict'
+
+export default {
+  name: 'ContactItem',
+  props: {
+    contact: { type: Object, required: true },
+    tags: { type: Array, required: true }
+  },
+  methods: {
+    getTag (tagId) {
+      return this.tags.find(t => t.id === tagId)
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+a {
+  text-decoration: none;
+  color: black;
+}
+</style>
--- pages/contacts/index.vue
+++ pages/contacts/index.vue
@@ -1,20 +1,46 @@
 <template>
-  <div>
-    <h1>Kontakte</h1>
-    <div v-for="contact in contacts" :key="contact.id">
-      <p>{{ contact.fName }} {{ contact.lName }}</p>
-      <img :src="contact.img" alt="">
+  <v-container>
+    <div class="display-4 ma-4 d-flex justify-center">
+      Alle Kontakte
     </div>
-  </div>
+    <v-sheet
+      class="mx-auto"
+      elevation="8"
+      max-width="1100"
+    >
+      <v-slide-group
+        v-model="model"
+        class="pa-4"
+        active-class="success"
+        show-arrows
+      >
+        <v-slide-item
+          v-for="contact in contacts"
+          :key="contact.id"
+        >
+          <ContactItem :contact="contact" :tags="tags" />
+        </v-slide-item>
+      </v-slide-group>
+    </v-sheet>
+  </v-container>
 </template>
 
 <script>
 'use strict'
 import { mapState } from 'vuex'
+import ContactItem from '@/components/contact-item'
+
 export default {
+  components: {
+    ContactItem
+  },
+  data: () => ({
+    model: null
+  }),
   computed: {
     ...mapState({
-      contacts: state => state.contacts.contacts
+      contacts: state => state.contacts.contacts,
+      tags: state => state.tags.tags
     })
   }
 }
