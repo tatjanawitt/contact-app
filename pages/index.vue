cb8ab334dd70d9ff096f89d573f785978ff4e115
RunawayTatti <tatjanawitt@gmx.de>
2020-03-30 17:52:57 +0200

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,19 @@
 <template>
-  <h1>Start Geburtstage anzeigen Suchen</h1>
+  <v-container>
+    <h1>Geburtstage</h1>
+    <v-row justify="center">
+      <v-expansion-panels inset>
+        <v-expansion-panel
+          v-for="(item,i) in 12"
+          :key="i"
+        >
+          <v-expansion-panel-header>Item</v-expansion-panel-header>
+          <v-expansion-panel-content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-row>
+    <v-container />
+  </v-container>
 </template>
