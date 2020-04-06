806790c012d87bffeb22554f0528191b37db02bb
RunawayTatti <tatjanawitt@gmx.de>
2020-04-06 18:59:51 +0200

    wip

--- layouts/navigation.vue
+++ layouts/navigation.vue
@@ -1,76 +1,50 @@
 <template>
   <div>
-    <v-app-bar flat text dense app>
+    <v-app-bar text app>
       <v-app-bar-nav-icon class="hidden-md-and-up" @click.stop="drawer = !drawer" />
       <v-layout class="hidden-sm-and-down">
-        <v-btn text to="/">
-          <v-icon left>
-            mdi-cake-variant
-          </v-icon> Geburtstage
+        <v-btn large text to="/">
+          <v-icon left v-text="'mdi-cake-variant'" />Geburtstage
         </v-btn>
-        <v-btn text to="/contacts">
-          <v-icon left>
-            mdi-account-group
-          </v-icon>
-          Kontakte
+        <v-btn large text to="/contacts">
+          <v-icon left v-text="'mdi-account-group'" />Kontakte
         </v-btn>
-        <v-btn v-if="$auth.user && $auth.user.admin" text to="/admin/contacts">
-          <v-icon left>
-            mdi-database-edit
-          </v-icon>
-          Admin
+        <v-btn v-if="$auth.user && $auth.user.admin" large text to="/admin/contacts">
+          <v-icon left v-text="'mdi-database-edit'" />Admin
         </v-btn>
       </v-layout>
       <v-spacer />
 
       <div v-if="$auth.loggedIn">
-        <v-btn text>
-          <v-icon left>
-            mdi-account
-          </v-icon>
-          {{ $auth.user.name }}
+        <v-btn large text>
+          <v-icon left v-text="'mdi-account'" />{{ $auth.user.name }}
         </v-btn>
-        <v-btn text @click="$auth.logout()">
-          Logout
-          <v-icon right>
-            mdi-logout-variant
-          </v-icon>
+        <v-btn large text @click="$auth.logout()">
+          Logout<v-icon right v-text="'mdi-logout-variant'" />
         </v-btn>
       </div>
 
       <div v-else>
-        <v-btn text to="/login">
-          <v-icon left>
-            mdi-login-variant
-          </v-icon>
-          Login
+        <v-btn large text to="/login">
+          <v-icon left v-text="'mdi-login-variant'" />Login
         </v-btn>
-        <v-btn text to="/register">
-          <v-icon left>
-            mdi-account-box-outline
-          </v-icon>
-          Register
+        <v-btn large text to="/register">
+          <v-icon left v-text="'mdi-account-box-outline'" />Register
         </v-btn>
       </div>
     </v-app-bar>
 
     <v-navigation-drawer v-model="drawer" absolute temporary>
-      <v-list nav dense>
-        <v-divider />
-        <v-list-item
-          v-for="item in links"
-          :key="item.icon"
-          :to="item.route"
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.text }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-divider />
+      <v-list class="d-flex flex-column">
+        <v-btn class="justify-start" large text to="/">
+          <v-icon left v-text="'mdi-cake-variant'" />Geburtstage
+        </v-btn>
+        <v-btn class="justify-start" large text to="/contacts">
+          <v-icon left v-text="'mdi-account-group'" />Kontakte
+        </v-btn>
+        <v-btn v-if="$auth.user && $auth.user.admin" class="justify-start" large text to="/admin/contacts">
+          <v-icon left v-text="'mdi-database-edit'" />Admin
+        </v-btn>
       </v-list>
     </v-navigation-drawer>
   </div>
@@ -80,12 +54,7 @@
 export default {
   data () {
     return {
-      drawer: false,
-      links: [
-        { text: 'Geburtstage', icon: 'mdi-cake-variant', route: '/' },
-        { text: 'Kontakte', icon: 'mdi-account-group', route: '/contacts' },
-        { text: 'Admin', icon: 'mdi-database-edit', route: '/admin/contacts' }
-      ]
+      drawer: false
     }
   }
 }
