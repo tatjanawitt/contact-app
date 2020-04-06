250f84eda7acdb2f31cb69580cc641af1dfac5a7
RunawayTatti <tatjanawitt@gmx.de>
2020-04-06 14:07:08 +0200

    wip

--- layouts/navigation.vue
+++ layouts/navigation.vue
@@ -1,80 +1,81 @@
 <template>
-  <nav>
-    <v-app-bar text dense app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn text to="/">
-        <v-icon left>
-          mdi-cake-variant
-        </v-icon> Geburtstage
-      </v-btn>
-      <v-btn text to="/contacts">
-        <v-icon left>
-          mdi-account-group
-        </v-icon>
-        Kontakte
-      </v-btn>
-      <v-btn v-if="$auth.user && $auth.user.admin" text to="/admin/contacts">
-        <v-icon left>
-          mdi-database-edit
-        </v-icon>
-        Admin
-      </v-btn>
-      <v-spacer />
-      <div v-if="$auth.loggedIn">
-        {{ $auth.user.email }}
-        <v-btn text @click="$auth.logout()">
-          Logout
-          <v-icon right>
-            mdi-logout-variant
-          </v-icon>
-        </v-btn>
-      </div>
-      <div v-else>
-        <v-btn text to="/login">
+  <div>
+    <v-app-bar flat text dense app>
+      <v-app-bar-nav-icon class="hidden-md-and-up" @click.stop="drawer = !drawer" />
+      <v-layout class="hidden-sm-and-down">
+        <v-btn text to="/">
           <v-icon left>
-            mdi-login-variant
-          </v-icon>
-          Login
+            mdi-cake-variant
+          </v-icon> Geburtstage
         </v-btn>
-        <v-btn text to="/register">
+        <v-btn text to="/contacts">
           <v-icon left>
-            mdi-account-box-outline
+            mdi-account-group
           </v-icon>
-          Register
+          Kontakte
         </v-btn>
-      </div>
+        <v-btn v-if="$auth.user && $auth.user.admin" text to="/admin/contacts">
+          <v-icon left>
+            mdi-database-edit
+          </v-icon>
+          Admin
+        </v-btn>
+        <v-spacer />
+        <div v-if="$auth.loggedIn">
+          {{ $auth.user.email }}
+          <v-btn text @click="$auth.logout()">
+            Logout
+            <v-icon right>
+              mdi-logout-variant
+            </v-icon>
+          </v-btn>
+        </div>
+        <div v-else>
+          <v-btn text to="/login">
+            <v-icon left>
+              mdi-login-variant
+            </v-icon>
+            Login
+          </v-btn>
+          <v-btn text to="/register">
+            <v-icon left>
+              mdi-account-box-outline
+            </v-icon>
+            Register
+          </v-btn>
+        </div>
+      </v-layout>
     </v-app-bar>
 
     <v-navigation-drawer
       v-model="drawer"
       absolute
-      bottom
+      left
       temporary
     >
       <v-list nav dense>
+        <v-divider />
         <v-list-item-group
           v-model="group"
           active-class="deep-purple--text text--accent-4"
         >
-          <v-list-item>
-            <v-list-item-title>Foo</v-list-item-title>
-          </v-list-item>
+          <v-list-item
+            v-for="item in links"
+            :key="item.icon"
+            :to="item.route"
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
 
-          <v-list-item>
-            <v-list-item-title>Bar</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-title>Fizz</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-title>Buzz</v-list-item-title>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.text }}</v-list-item-title>
+            </v-list-item-content>
           </v-list-item>
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
-  </nav>
+  </div>
   <!--v-row>
     <v-btn text to="/">
       <v-icon left>
@@ -131,7 +132,7 @@ export default {
         { text: 'Admin', icon: 'mdi-database-edit', route: '/admin/contacts' },
         { text: 'Login', icon: 'mdi-login-variant', route: '/login' },
         { text: 'Register', icon: ' mdi-account-box-outline', route: '/register' },
-        { text: 'Logout', icon: 'mdi-database-edit', route: '/' }
+        { text: 'Logout', icon: 'mdi-logout-variant', route: '/' }
       ],
       mini: true,
       group: null
