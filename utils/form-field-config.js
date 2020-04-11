09d9c6bcd66af77716cf78668afc291f0b2326bc
RunawayTatti <tatjanawitt@gmx.de>
2020-04-11 12:11:39 +0200

    wip

--- utils/form-field-config.js
+++ utils/form-field-config.js
@@ -1,7 +1,6 @@
 import v from '@/utils/validations'
 const contactForm = [
   {
-    text: 'Vorname*',
     value: 'fName',
     icon: 'mdi-account-heart',
     max: '30',
@@ -9,7 +8,6 @@ const contactForm = [
     rules: [v.required(), v.minLength(3)]
   },
   {
-    text: 'Nachname*',
     value: 'lName',
     icon: 'mdi-human-male-boy',
     max: '30',
@@ -17,50 +15,42 @@ const contactForm = [
     rules: [v.required(), v.minLength(3)]
   },
   {
-    text: 'Email*',
     value: 'email',
     icon: 'mdi-email',
     max: '50',
     rules: [v.required(), v.emailFormat()]
   },
   {
-    text: 'Telefon',
     value: 'fon',
     icon: 'mdi-phone',
     max: '30'
   },
   {
-    text: 'Mobil',
     value: 'mobil',
     icon: 'mdi-cellphone-wireless',
     max: '30'
   },
   {
-    text: 'Strasse',
     value: 'street',
     icon: 'mdi-home',
     max: '50'
   },
   {
-    text: 'Postleitzahl',
     value: 'zip',
     icon: 'mdi-map-marker',
     max: '5',
     rules: [v.zipFormat()]
   },
   {
-    text: 'Ort',
     value: 'place',
     icon: 'mdi-home-city',
     max: '50'
   },
   {
-    text: 'Bild (URL)',
     value: 'img',
     icon: 'mdi-file-image-outline'
   },
   {
-    text: 'Bewertung (1-5)',
     value: 'rating',
     icon: 'mdi-heart',
     max: '1',
