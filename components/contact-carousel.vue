c80f141db6ad9a59585a2c9af03f2f5f6bf79054
RunawayTatti <tatjanawitt@gmx.de>
2020-04-26 18:17:19 +0200

    wip

--- components/contact-carousel.vue
+++ components/contact-carousel.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <div class="display-2 my-6 center" v-text="content.title" />
-    <div class="subtitle-1 my-4 d-flex justify-center" v-text="content.sub" />
+    <div class="subtitle-1 my-4 d-flex justify-center" v-html="content.sub" />
     <v-layout elevation-14>
       <v-carousel v-if="content.images.length" cycle hide-delimiters>
         <v-carousel-item v-for="(img,i) in content.images" :key="i" :src="img.src" />
