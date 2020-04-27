e2c9ca6943ada60b7fcb2f9cafe1c980cb458f38
RunawayTatti <tatjanawitt@gmx.de>
2020-04-27 17:44:06 +0200

    whoops page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,44 +1,48 @@
 <template>
-  <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
-  </v-app>
+  <v-container class="text-center" fill-height style="height: calc(100vh - 58px);">
+    <v-row align="center">
+      <v-col class="d-flex justify-center">
+        <v-card class="pa-3" max-width="400px">
+          <div v-if="error.statusCode === 404">
+            <h1 class="display-2 primary--text">
+              {{ $t('errorSite.notFound') }}
+            </h1>
+            <p>{{ $t('errorSite.notFoundMsg') }}</p>
+          </div>
+          <div v-else>
+            <h1 class="display-2 primary--text">
+              {{ $t('errorSite.title') }}
+            </h1>
+            <p>{{ $t('errorSite.msg') }}</p>
+          </div>
+          <v-btn to="/" color="primary" outlined>
+            {{ $t('errorSite.btnText') }}
+          </v-btn>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
   layout: 'empty',
   props: {
-    error: {
-      type: Object,
-      default: null
-    }
+    error: { type: Object, default: null }
   },
   data () {
     return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
+      pageNotFound: this.$t('errorSite.notFound'),
+      otherError: this.$t('errorSite.title')
     }
   },
   head () {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
-    return {
-      title
-    }
+    const title = this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+    return { title }
   }
 }
 </script>
 
 <style scoped>
-h1 {
-  font-size: 20px;
-}
+h1 { font-size: 20px; }
 </style>
